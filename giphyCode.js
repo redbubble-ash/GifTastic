9659be89dfadfc21c11c66646daa8b29269e2f0b
Jing Xie <xiejing418@gmail.com>
2019-04-30 00:17:10 -0700

    added js html with Giphy API

---
+++ giphyCode.js
@@ -0,0 +1,59 @@
+
+$(document).ready(function () {
+
+    var topics = ["Dubai", "Beijing", "Shanghai", "Hongkong", "Sydney", "Moscow", "London", "Seattle", "New York", "Rome", "Miami", "Barcelona", "Paris"];
+
+    for (i = 0; i < topics.length; i++) {
+
+        $("#button").append("<button city=" + topics[i] + ">" + topics[i] + "</button>" + " ");
+
+
+
+
+    };
+
+    $("button").on("click", function () {
+        var travelCity = $(this).attr("city");
+
+        var queryURL = queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            travelCity + "&api_key=LHm5r26Y8UndONHQM4SKc1OHY96mKU8U&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                var results = response.data;
+
+                for (var i = 0; i < results.length; i++) {
+                    var gifDiv = $("<div>");
+
+                    var rating = results[i].rating;
+
+                    var p = $("<p>").text("Rating: " + rating);
+
+                    var cityImage = $("<img>");
+                    cityImage.attr("src", results[i].images.fixed_height.url);
+
+                    gifDiv.prepend(p);
+                    gifDiv.prepend(cityImage);
+
+                    $("#gifs-appear-here").prepend(gifDiv);
+
+                };
+
+
+
+
+
+
+
+            });
+
+
+
+
+
+    })
+
+})
